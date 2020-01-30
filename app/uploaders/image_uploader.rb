5deb118369054e7a1359242d59c746be152ab7bf
kazuki13070311 <kazuki13070311@gmail.com>
2020-01-30 20:31:08 +0900

    マイページ編集画面にファイル選択画面実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_limit: [700,700]
+  process resize_to_limit: [200,200]
+  process resize_to_fit: [200, 200]
 
   #保存形式をJPGにする
   process :convert => 'jpg'
@@ -12,9 +13,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [200, 200]
   end
 
-  #version :thumb50 do
-    #process resize_to_fit: [100,100]
-  #end
+  version :thumb50 do
+    process resize_to_fit: [100,100]
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -22,17 +23,18 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
+  #def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #  "default.png"
+  #  "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
