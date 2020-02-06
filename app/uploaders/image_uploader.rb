f16c9d73ae3ee4c5cb0ad1237fc3d8d2f2a02d7b
kazuki13070311 <kazuki13070311@gmail.com>
2020-02-06 21:38:17 +0900

    N＋1問題解決

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   # def default_url
   # For Rails 3.1+ asset pipeline compatibility:
-  # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #  ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     "default.jpg"
   # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
