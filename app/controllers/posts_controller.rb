9b93b13f7b1c95d5d219657af49eda81c0a291d2
kazuki13070311 <kazuki13070311@gmail.com>
2020-02-24 23:36:53 +0900

    👍投稿機能修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,16 +12,21 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_back(fallback_location: root_path)
+      redirect_to posts_path, notice: '投稿が完了しました。'
     else
-      redirect_back(fallback_location: root_path)
+      redirect_to posts_path, notice: '投稿できませんでした。全項目入力してください。 '
     end
+      
+      #redirect_back(fallback_location: root_path)
+    #else
+      #redirect_back(fallback_location: root_path)
+    #end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to root_path, notice: '募集内容を削除しました。'
+    redirect_to posts_path, notice: '募集内容を削除しました。'
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :content, presence: true 
+  validates :image, presence: true
+
 end
