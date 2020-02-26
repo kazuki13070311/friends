a27ce2dd052acd92b1bda59fc5a1ee9891e4fe31
kazuki13070311 <kazuki13070311@gmail.com>
2020-02-26 22:53:53 +0900

    🔧postsテーブルにdescriptinカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_130124) do
+ActiveRecord::Schema.define(version: 2020_02_26_135124) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_130124) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.text "description"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
