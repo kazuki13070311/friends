d75e46fe733b9ca181eb4c10761d63347f08a6da
kazuki13070311 <kazuki13070311@gmail.com>
2020-03-07 01:00:17 +0900

    😔いいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
       @post = Post.find(params[:post_id])
       like = current_user.likes.create(post_id: params[:post_id])
       like.save
-      #redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path)
     end
 
     def destroy
       @post = Post.find(params[:post_id])
       like = Like.find_by(post_id: params[:post_id],user_id: current_user.id)
       like.destroy
-      #redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,8 @@ class Post < ApplicationRecord
   validates :description, presence: true
 
   belongs_to :user
-  has_many :likes
+  has_many :likes,dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   mount_uploader :image, ImageUploader
-
 end
