eaa2f7a0323e20238eb402bb106a3d3b3f4103ed
kazuki13070311 <kazu_0311@icloud.com>
2021-04-20 13:48:02 +0000

    [Add]ログローテーション追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,4 +114,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.logger = Logger.new("log/production.log", 'weekly')
 end
