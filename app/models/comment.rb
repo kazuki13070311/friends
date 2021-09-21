2319892b5b6596ce1eb5148a4811d19c5b89f4ab
kazuki13070311 <kazu_0311@icloud.com>
2021-09-21 18:01:15 +0900

    [Fix]通知機能修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,25 +16,4 @@ class Comment < ApplicationRecord
 
   validates :content, presence: true
   validates :content, length: { maximum: 200}
-
-  def create_notification_comment!(current_user, comment_id)
-    temp_ids = Comment.select(:user_id).where(friend_id: friend_id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
-    end
-    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
-  end
-
-  def save_notification_comment!(current_user, comment_id, visited_id)
-    notification = current_user.active_notifications.new(
-      friend_id: friend_id,
-      comment_id: comment_id,
-      visited_id: visited_id,
-      action: 'comment'
-    )
-    if notification.visitor_id == notification.visited_id
-      notification.checked = true
-    end
-    notification.save if notification.valid?
-  end
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -23,6 +23,27 @@ class Friend < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(friend_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      friend_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
+
   private
 
   def validate_name_not_including_comma
